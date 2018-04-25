021f20b84b97a5ba07d4ed3351b41188c1a87710
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-04-25 19:37:55 +0800

    Add user details for later access in displaying information

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Logo from './components/Logo';
 
+const user = {
+  firstName: 'Regina Alyssa',
+  lastName: 'Esguerra',
+  position: 'Full-Stack Developer',
+  email: 'reginaalyssa01809@gmail.com',
+  mobile: '09176206021',
+  github: 'github.com/reginaalyssa01809'
+};
+
 class App extends Component {
   render() {
     return (
