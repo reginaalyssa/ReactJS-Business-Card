fd342c7954a3f9c6a914a240e0a150fa3cdadf0b
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-04-25 20:59:16 +0800

    Create new BusinessCard component and move all related content inside it

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import emailLogo from './assets/email.png';
-import mobileLogo from './assets/phone-call.png';
-import githubLogo from './assets/github.png';
 import './App.css';
-import ContactInfo from './components/ContactInfo';
-import Logo from './components/Logo';
-import Name from './components/Name';
-import WorkInfo from './components/WorkInfo';
-
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Regina Alyssa',
-  lastName: 'Esguerra',
-  position: 'Full-Stack Developer',
-  email: 'reginaalyssa01809@gmail.com',
-  mobile: '+63916 123 4567',
-  github: 'github.com/reginaalyssa01809'
-};
+import BusinessCard from './components/BusinessCard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Business-card">
-          <div className="Logo-container">
-            <Logo logo={logo}/>
-          </div>
-          <div className="Basic-info">
-            <Name name={formatName(user)}/>
-            <WorkInfo position={user.position}/>
-            <div className="Contact-info">
-              <ContactInfo logo={emailLogo} value={user.email} type="E-mail" />
-              <ContactInfo logo={githubLogo} value={user.github} type="GitHub" />
-              <ContactInfo logo={mobileLogo} value={user.mobile} type="Mobile" />
-            </div>
-          </div>
-        </div>
+        <BusinessCard />
       </div>
     );
   }
---
+++ src/components/BusinessCard.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import logo from './../logo.svg';
+import emailLogo from './../assets/email.png';
+import mobileLogo from './../assets/phone-call.png';
+import githubLogo from './../assets/github.png';
+import ContactInfo from './ContactInfo';
+import Logo from './Logo';
+import Name from './Name';
+import WorkInfo from './WorkInfo';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Regina Alyssa',
+  lastName: 'Esguerra',
+  position: 'Full-Stack Developer',
+  email: 'reginaalyssa01809@gmail.com',
+  mobile: '+63916 123 4567',
+  github: 'github.com/reginaalyssa01809'
+};
+
+class BusinessCard extends Component {
+  render() {
+    return (
+      <div className="Business-card">
+        <div className="Logo-container">
+          <Logo logo={logo}/>
+        </div>
+        <div className="Basic-info">
+          <Name name={formatName(user)}/>
+          <WorkInfo position={user.position}/>
+          <div className="Contact-info">
+            <ContactInfo logo={emailLogo} value={user.email} type="E-mail" />
+            <ContactInfo logo={githubLogo} value={user.github} type="GitHub" />
+            <ContactInfo logo={mobileLogo} value={user.mobile} type="Mobile" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BusinessCard
