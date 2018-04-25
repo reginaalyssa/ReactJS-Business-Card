5566f22525ef6da7761d6f6812c7a4730963bebd
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-04-25 19:34:09 +0800

    Create new Logo component, add styles to logo placement, and include it in business card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Logo from './components/Logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="Business-card">
+          <div className="Logo-container">
+            <Logo logo={logo}/>
+          </div>
         </div>
       </div>
     );
---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Logo extends Component {
+	render() {
+		return(
+			<img src={this.props.logo} className="App-logo" alt="logo" />
+		)
+	}
+}
+
+export default Logo
