47ca5b1828337613d9bb1ec3eab33abc0a5c7a5c
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-04-27 01:26:53 +0800

    Add back missing semicolons

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -4,8 +4,8 @@ class ContactInfo extends Component {
 	render() {
 		return(
 			<h3><img src={this.props.logo} alt="{this.props.type} icon" className="Contact-icon" /> {this.props.value}</h3>
-		)
+		);
 	}
 }
 
-export default ContactInfo
+export default ContactInfo;
--- src/components/Name.js
+++ src/components/Name.js
@@ -4,8 +4,8 @@ class Name extends Component {
 	render() {
 		return(
 			<h1>{this.props.name}</h1>
-		)
+		);
 	}
 }
 
-export default Name
+export default Name;
