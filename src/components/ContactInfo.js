ecf89443142d5ed75f387b4a56315a37b1b9286d
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-04-26 16:00:15 +0800

    Change class to use className in ContactInfo img

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class ContactInfo extends Component {
 	render() {
 		return(
-			<h3><img src={this.props.logo} alt="{this.props.type} icon" class="Contact-icon" /> {this.props.value}</h3>
+			<h3><img src={this.props.logo} alt="{this.props.type} icon" className="Contact-icon" /> {this.props.value}</h3>
 		)
 	}
 }
