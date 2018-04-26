ce9be2464f2eb0feb12b831e4752e4b1fc8463d2
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-04-26 17:51:08 +0800

    Use state from parent App to determine night mode instead of from ModeButton directly

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import BusinessCard from './components/BusinessCard';
 import ModeButton from './components/ModeButton';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { 
+        isNightMode: false 
+      };
+  }
+
+  changeMode = () => {
+    this.setState((prevState) => {
+      return {isNightMode: !prevState.isNightMode};
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <BusinessCard />
-        <ModeButton />
+        <BusinessCard className={this.state.isNightMode ? "Night-mode" : ""} />
+        <ModeButton changeMode={this.changeMode} isNightMode={this.state.isNightMode}/>
       </div>
     );
   }
--- src/components/ModeButton.js
+++ src/components/ModeButton.js
@@ -1,22 +1,9 @@
 import React, {Component} from 'react';
 
 class ModeButton extends Component {
-  constructor(props) {
-      super(props);
-      this.state = { 
-        isNightMode: false 
-      };
-  }
-
-  changeMode = () => {
-    this.setState((prevState) => {
-      return {isNightMode: !prevState.isNightMode};
-    });
-  }
-
   render() {
     return(
-      <button onClick={this.changeMode}>{this.state.isNightMode === true ? "Day Mode" : "Night Mode"}</button>
+      <button onClick={this.props.changeMode}>{this.props.isNightMode === true ? "Day Mode" : "Night Mode"}</button>
     )
   }
 }
