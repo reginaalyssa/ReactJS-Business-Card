a62f2779e7738c40dfd31dbbfd8aa01bb9f24642
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-05-19 14:43:53 +0800

    Refactor Navbar to become a presentational component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import emailLogo from './../../assets/wwcodemanila.png';
 
-
-class Navbar extends Component {
-	render() {
-		return(
-			<div className="Navbar">
-				<img src={emailLogo} alt="Women Who Code Manila icon" className="Women-Who-Code-icon" />
-			</div>
-		);
-	}
-}
+const Navbar = () => (
+	<div className="Navbar">
+		<img src={emailLogo} alt="Women Who Code Manila icon" className="Women-Who-Code-icon" />
+	</div>
+)
 
 export default Navbar;
